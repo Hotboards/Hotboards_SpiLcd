acecbca076bb5ffa56d8447c54c3bc61f610aae0
Diego Perez <diego.perez@hotboards.org>
2016-03-05 19:34:41 +0000

    move fucntions comment to header file

--- Hotboards_SpiLcd.cpp
+++ Hotboards_SpiLcd.cpp
@@ -29,9 +29,6 @@ Hotboards_SpiLcd::Hotboards_SpiLcd( int cs, int rs, int rst )
     _rst_pin = rst;
 }
 
-/*
- * send commands to initialize internal lcd controller
- */
 void Hotboards_SpiLcd::begin( void )
 {
     digitalWrite( _cs_pin, HIGH );
@@ -69,58 +66,36 @@ void Hotboards_SpiLcd::begin( void )
     clear( );/*clearscreen*/
 }
 
-/*
- * Clears the LCD screen and positions the cursor in the upper-left corner. 
- */
 void Hotboards_SpiLcd::clear( void )
 {
     command( HT_SPILCD_CLEARDISPLAY );  // clear display, set cursor position to zero
     delayMicroseconds( 2000 );          // this command takes a long time!   
 }
 
-/*
- * Positions the cursor in the upper-left of the LCD. That is, use that location 
- * in outputting subsequent text to the display. To also clear the display, use 
- * the clear() function instead
- */
 void Hotboards_SpiLcd::home( void )
 {
     command( HT_SPILCD_RETURNHOME );      // set cursor position to zero
     delayMicroseconds( 2000 );          // this command takes a long time!   
 }
 
-/*
- * Turns off the LCD display, without losing the text currently shown on it.
- */
 void Hotboards_SpiLcd::noDisplay( void )
 {
     _displaycontrol &= ~HT_SPILCD_DISPLAYON;
     command( HT_SPILCD_DISPLAYCONTROL | _displaycontrol );
 }
 
-/*
- * Turns on the LCD display, after it's been turned off with noDisplay(). 
- * This will restore the text (and cursor) that was on the display. 
- */
 void Hotboards_SpiLcd::display( void )
 {
     _displaycontrol |= HT_SPILCD_DISPLAYON;
     command( HT_SPILCD_DISPLAYCONTROL | _displaycontrol );  
 }
 
-/*
- * Turns off the blinking LCD cursor. 
- */
 void Hotboards_SpiLcd::noBlink( void )
 {
     _displaycontrol &= ~HT_SPILCD_BLINKON;
     command( HT_SPILCD_DISPLAYCONTROL | _displaycontrol );
 }    
     
-/*
- * Display the blinking LCD cursor. If used in combination with the cursor() function, 
- * the result will depend on the particular display. 
- */
 void Hotboards_SpiLcd::blink( void )
 {
     _displaycontrol |= HT_SPILCD_BLINKON;
@@ -128,19 +103,12 @@ void Hotboards_SpiLcd::blink( void )
 }
 
 
-/*
- * Hides the LCD cursor.
- */
 void Hotboards_SpiLcd::noCursor( void )
 {
     _displaycontrol &= ~HT_SPILCD_CURSORON;
     command( HT_SPILCD_DISPLAYCONTROL | _displaycontrol );
 }
 
-/*
- * Display the LCD cursor: an underscore (line) at the position to which the 
- * next character will be written. 
- */
 void Hotboards_SpiLcd::cursor( void )
 {
     _displaycontrol |= HT_SPILCD_CURSORON;
@@ -148,73 +116,40 @@ void Hotboards_SpiLcd::cursor( void )
   
 }
 
-/*
- * Scrolls the contents of the display (text and cursor) one space to the left. 
- */
 void Hotboards_SpiLcd::scrollDisplayLeft( void )
 {
     command( HT_SPILCD_CURSORSHIFT | HT_SPILCD_DISPLAYMOVE | HT_SPILCD_MOVELEFT );
 }
 
-/*
- * Scrolls the contents of the display (text and cursor) one space to the right. 
- */
 void Hotboards_SpiLcd::scrollDisplayRight( void )
 {
     command( HT_SPILCD_CURSORSHIFT | HT_SPILCD_DISPLAYMOVE | HT_SPILCD_MOVERIGHT );  
 }
 
-/*
- * Set the direction for text written to the LCD to left-to-right, the default. 
- * This means that subsequent characters written to the display will go from left to right, 
- * but does not affect previously-output text. 
- */
 void Hotboards_SpiLcd::leftToRight( void )
 {
     _displaymode |= HT_SPILCD_ENTRYLEFT;
     command( HT_SPILCD_ENTRYMODESET | _displaymode );
 }
 
-/*
- * Set the direction for text written to the LCD to right-to-left (the default is left-to-right). 
- * This means that subsequent characters written to the display will go from right to left, 
- * but does not affect previously-output text. 
- */
 void Hotboards_SpiLcd::rightToLeft( void )
 {
     _displaymode &= ~HT_SPILCD_ENTRYLEFT;
     command( HT_SPILCD_ENTRYMODESET | _displaymode );  
 }
 
-/*
- * Turns on automatic scrolling of the LCD. This causes each character 
- * output to the display to push previous characters over by one space. 
- * If the current text direction is left-to-right (the default), 
- * the display scrolls to the left; if the current direction is right-to-left, 
- * the display scrolls to the right. This has the effect of outputting 
- * each new character to the same location on the LCD. 
- */
 void Hotboards_SpiLcd::autoscroll( void )
 {
     _displaymode |= HT_SPILCD_ENTRYSHIFTINCREMENT;
     command( HT_SPILCD_ENTRYMODESET | _displaymode );
 }
 
-/*
- * Turns off automatic scrolling of the LCD.
- */
 void Hotboards_SpiLcd::noAutoscroll( void )
 {
     _displaymode &= ~HT_SPILCD_ENTRYSHIFTINCREMENT;
     command( HT_SPILCD_ENTRYMODESET | _displaymode );   
 }
     
-/*
- * Position the LCD cursor; that is, set the location at which subsequent 
- * text written to the LCD will be displayed.
- * col: the column at which to position the cursor (with 0 being the first column)
- * row: the row at which to position the cursor (with 0 being the first row) 
- */
 void Hotboards_SpiLcd::setCursor( uint8_t col, uint8_t row )
 {
     const uint8_t Buffer[ 2 ]= { 0x00u, 0x40u };
--- Hotboards_SpiLcd.h
+++ Hotboards_SpiLcd.h
@@ -52,33 +52,220 @@
 #define HT_SPILCD_NORMINST              0x00
 
 
+/** Hotboards_SpiLcd class.
+ *  Used to control lcd with spi serial interface
+ *
+ * Example:
+ * @code
+ * #include <Arduino.h>
+ * #include <Hotboards_SpiLcd.h>
+ *
+ * Hotboards_SpiLcd lcd( 7, 6, 5 ); //cs, rs, rst
+ * 
+ * void setup( void ) {
+ *     SPI.begin( );
+ *     lcd.begin( );
+ *     lcd.print( "Hola mundo" );
+ * }
+ * 
+ * @endcode
+ */
 class Hotboards_SpiLcd : public Print
 {
   public:
   
+    /** Create Hotboards_SpiLcd instance
+      * @param cs pin to control the chip select signal
+      * @param rs pin to control the data/command signal
+      * @param rst pin to control the reset signal
+      *
+      * Example:
+      * @code
+      *   // then we can create the instance
+      *   Hotboards_SpiLcd lcd( 7, 6, 5 ); //cs, rs, rst
+      * @endcode
+      */
     Hotboards_SpiLcd( int cs, int rs, int rst );
+    
+    /** Send commands to initialize internal lcd controller
+      *
+      * Example:
+      * @code
+      *   // After create the instance we init
+      *   // but we need to init the SPI first
+      *   SPI.begin();
+      *   lcd.begin();
+      * @endcode
+      */
     void begin( void );
+    
+    /** Clears the LCD screen and positions the cursor in the upper-left corner. 
+      *
+      * Example:
+      * @code
+      *   lcd.clear();
+      * @endcode
+      */
     void clear( void );
+    
+    /** Positions the cursor in the upper-left of the LCD. That is, use that location 
+      * in outputting subsequent text to the display. To also clear the display, use 
+      * the clear() function instead
+      *
+      * Example:
+      * @code
+      *   lcd.home();
+      * @endcode
+      */
     void home( void );
 
+    /** Turns off the LCD display, without losing the text currently shown on it.
+      *
+      * Example:
+      * @code
+      *   lcd.noDisplay();
+      * @endcode
+      */
     void noDisplay( void );
+    
+    /** Turns on the LCD display, after it's been turned off with noDisplay(). 
+      * This will restore the text (and cursor) that was on the display. 
+      *
+      * Example:
+      * @code
+      *   lcd.display();
+      * @endcode
+      */
     void display( void );
+    
+    /** Turns off the blinking LCD cursor. 
+      *
+      * Example:
+      * @code
+      *   lcd.noBlink();
+      * @endcode
+      */
     void noBlink( void );
+    
+    /** Display the blinking LCD cursor. If used in combination with the cursor() function, 
+      * the result will depend on the particular display. 
+      *
+      * Example:
+      * @code
+      *   lcd.blink();
+      * @endcode
+      */
     void blink( void );
+    
+    /** Hides the LCD cursor.
+      *
+      * Example:
+      * @code
+      *   lcd.noCursor();
+      * @endcode
+      */
     void noCursor( void );
+    
+    /** Display the LCD cursor: an underscore (line) at the position to which the 
+      * next character will be written. 
+      *
+      * Example:
+      * @code
+      *   lcd.cursor();
+      * @endcode
+      */
     void cursor( void );
+    
+    /** Scrolls the contents of the display (text and cursor) one space to the left. 
+      *
+      * Example:
+      * @code
+      *   lcd.scrollDisplayLeft();
+      * @endcode
+      */
     void scrollDisplayLeft( void );
+    
+    /** Scrolls the contents of the display (text and cursor) one space to the right. 
+      *
+      * Example:
+      * @code
+      *   lcd.scrollDisplayRight();
+      * @endcode
+      */
     void scrollDisplayRight( void );
+    
+    /** Set the direction for text written to the LCD to left-to-right, the default. 
+      * This means that subsequent characters written to the display will go from left to right, 
+      * but does not affect previously-output text. 
+      *
+      * Example:
+      * @code
+      *   lcd.leftToRight();
+      * @endcode
+      */
     void leftToRight( void );
+    
+    /** Set the direction for text written to the LCD to right-to-left (the default is left-to-right). 
+      * This means that subsequent characters written to the display will go from right to left, 
+      * but does not affect previously-output text. 
+      *
+      * Example:
+      * @code
+      *   lcd.rightToLeft();
+      * @endcode
+      */
     void rightToLeft( void );
+    
+    /** Turns on automatic scrolling of the LCD. This causes each character 
+      * output to the display to push previous characters over by one space. 
+      * If the current text direction is left-to-right (the default), 
+      * the display scrolls to the left; if the current direction is right-to-left, 
+      * the display scrolls to the right. This has the effect of outputting 
+      * each new character to the same location on the LCD. 
+      *
+      * Example:
+      * @code
+      *   lcd.autoscroll();
+      * @endcode
+      */
     void autoscroll( void );
+    
+    /** Turns off automatic scrolling of the LCD.
+      *
+      * Example:
+      * @code
+      *   lcd.noAutoscroll();
+      * @endcode
+      */
     void noAutoscroll( void );
 
     //void setRowOffsets( int row1, int row2 );
     //void createChar( uint8_t, uint8_t[] );
+    
+    /** Position the LCD cursor; that is, set the location at which subsequent 
+      * text written to the LCD will be displayed.
+      * @param col: the column at which to position the cursor (with 0 being the first column)
+      * @param row: the row at which to position the cursor (with 0 being the first row) 
+      *
+      * Example:
+      * @code
+      *   lcd.setCursor( 3, 1 );
+      * @endcode
+      */
     void setCursor( uint8_t, uint8_t ); 
-    virtual size_t write( uint8_t );
+    
+    /** Send a command to the LCD.
+      * @data: cmd command to send
+      *
+      * Example:
+      * @code
+      *   // clear screen command
+      *   lcd.command( 0x30 );
+      * @endcode
+      */ 
     void command( uint8_t );
+    
+    virtual size_t write( uint8_t );
     
     using Print::write;
   
